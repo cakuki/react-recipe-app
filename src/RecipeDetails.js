1c8285b36226aa91eafb84f42040b26f13877d53
Can Kinay <me@ckk.im>
2022-12-29 01:38:10 +0100

    feat: fetch images from unsplash

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./RecipeDetails.css";
 
 const RecipeDetails = ({ recipe, onFavoriteClick, onBackClick, favorite }) => {
-  const { name, ingredients, price, image } = recipe;
+  const { name, ingredients, price } = recipe;
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/search/photos?query=${recipe.name}&client_id=${process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY}`
+        );
+        const data = await response.json();
+        setImageUrl(data.results[0].urls.small);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchImage();
+  }, [recipe]);
 
   return (
     <div className="recipe-details">
-      <img src={image} alt={name} />
+      <img src={imageUrl} alt={name} />
       <h2>{name}</h2>
-      <p>Ingredients: {ingredients}</p>
+      <h3>Ingredients:</h3>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient}</li>
+        ))}
+      </ul>
       <p>Price: ${price}</p>
       <button onClick={() => onFavoriteClick(recipe.id)}>
         {favorite ? "Remove from" : "Add to"} favorites
--- src/recipes.js
+++ src/recipes.js
@@ -2,38 +2,98 @@ const recipes = [
   {
     id: 1,
     name: "Spaghetti Bolognese",
-    ingredients:
-      "Spaghetti, ground beef, tomatoes, onions, garlic, red wine, salt, pepper, olive oil",
+    ingredients: [
+      "Spaghetti",
+      "ground beef",
+      "tomatoes",
+      "onions",
+      "garlic",
+      "red wine",
+      "salt",
+      "pepper",
+      "olive oil",
+    ],
     price: 15.0,
-    image:
-      "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2020/06/spaghetti-bolognese.jpg",
   },
   {
     id: 2,
     name: "Lasagna",
-    ingredients:
-      "Lasagna noodles, ground beef, tomato sauce, mozzarella cheese, parmesan cheese, salt, pepper, olive oil",
+    ingredients: [
+      "Lasagna noodles",
+      "ground beef",
+      "tomato sauce",
+      "mozzarella cheese",
+      "parmesan cheese",
+      "salt",
+      "pepper",
+      "olive oil",
+    ],
     price: 20.0,
-    image:
-      "https://www.thespruceeats.com/thmb/X5qMkOstlQGUhKU6l3BM3qfU6Zg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/lasagna-recipe-2994710-Hero-5bb0b4d0c9e77c0051f00121.jpg",
   },
   {
     id: 3,
     name: "Pizza Margherita",
-    ingredients:
-      "Pizza dough, tomatoes, mozzarella cheese, basil, salt, pepper, olive oil",
+    ingredients: [
+      "Pizza dough",
+      "tomatoes",
+      "mozzarella cheese",
+      "basil",
+      "salt",
+      "pepper",
+      "olive oil",
+    ],
     price: 12.0,
-    image:
-      "https://www.seriouseats.com/recipes/images/2015/07/20150728-homemade-pizza-dough-recipe-food-lab-11.jpg",
   },
   {
     id: 4,
     name: "Chicken Parmesan",
-    ingredients:
-      "Chicken breasts, breadcrumbs, parmesan cheese, mozzarella cheese, eggs, salt, pepper, olive oil",
+    ingredients: [
+      "Chicken breasts",
+      "breadcrumbs",
+      "parmesan cheese",
+      "mozzarella cheese",
+      "eggs",
+      "salt",
+      "pepper",
+      "olive oil",
+    ],
     price: 18.0,
-    image:
-      "https://www.thespruceeats.com/thmb/L-Z_npF-A5Y3q5z5hgLSyI5NjBY=/2121x1414/smart/filters:no_upscale()/chicken-parmesan-3056854-Hero-5c05dd8346e0fb00019b9f7b.jpg",
+  },
+  {
+    id: 6,
+    name: "Thai Red Curry",
+    ingredients: [
+      "chicken",
+      "coconut milk",
+      "red curry paste",
+      "vegetables",
+      "basmati rice",
+    ],
+    price: 15,
+  },
+  {
+    id: 7,
+    name: "Pancakes",
+    ingredients: ["flour", "milk", "eggs", "sugar", "baking powder"],
+    price: 5,
+  },
+  {
+    id: 8,
+    name: "Salmon with Roasted Vegetables",
+    ingredients: ["salmon fillets", "olive oil", "vegetables", "lemon", "dill"],
+    price: 20,
+  },
+  {
+    id: 9,
+    name: "Beef Stir-Fry",
+    ingredients: [
+      "beef sirloin",
+      "vegetables",
+      "soy sauce",
+      "ginger",
+      "noodles",
+    ],
+    price: 12,
   },
 ];
 
